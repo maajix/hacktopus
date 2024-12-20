b7c47a5e5ac154eff60025fbdd9da67ea7ac0102
Max Randhahn <49589091+maajix@users.noreply.github.com>
2024-12-20 01:23:22 +0100

    Refactor

--- src/process/CLIExecutionManager.py
+++ src/process/CLIExecutionManager.py
@@ -4,19 +4,32 @@ import subprocess
 
 
 class CommandExecutionManager:
+    """
+    A class to manage the execution of command line commands
+    """
     @staticmethod
-    def run(command, stream_output=True):
-        """Run a command with arguments and stream the output to the console if enabled"""
+    def run(command: str, stream_live_output: bool = True):
+        """
+        Run a command with arguments and stream the output to the console if enabled
+
+        :param command: The command to run
+        :param stream_live_output: Stream the live output to the console
+        :return: The return code of the command
+
+        :Example:
+        >>> CommandExecutionManager.run("echo 'Hello World'")
+        Hello World
+        """
         try:
             _proc_stdout = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
             while True:
                 output = _proc_stdout.stdout.readline()
                 if _proc_stdout.poll() is not None:
                     break
-                if stream_output and output:
+                if stream_live_output and output:
                     print(output.strip().decode(encoding="utf-8"))
             return _proc_stdout.poll()
         except FileNotFoundError:
-            return f"[ERR] Error while trying to run command '{command}'"
+            return f"[ERR] Error while trying to run command '{command}'"  # @TODO: Check if return works with threading
         except Exception as e:
             return e
